bb63bb5f9a6134c22dcbb3e2f62bed7df1ce975c
dangquocdung <dungthinhvn@gmail.com>
2020-05-13 07:27:38 +0700

    ádsad

--- resources/views/vue/index.blade.php
+++ resources/views/vue/index.blade.php
@@ -6,9 +6,9 @@
 <!-- Logo -->
 <a href="#" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
-    <span class="logo-mini"><b>ĐQD</b></span>
+    <span class="logo-mini"><b>dungthinh    </b></span>
     <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg"><b>ĐQD</b></span>
+    <span class="logo-lg"><b>dungthinh</b></span>
 </a>
 <!-- Header Navbar: style can be found in header.less -->
 <nav class="navbar navbar-static-top">
